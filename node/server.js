a3baeb6ac7fa2db830fc72a0829dbc583574c131
Robert <464554492@qq.com>
2017-08-22 21:26:46 +0800

    url跨域

--- node/server.js
+++ node/server.js
@@ -1,13 +1,18 @@
 const http = require('http');
+const fs = require('fs');
 
-const server = http.createServer((req, res) => {
+const server = http.createServer((request, response) => {
 
     console.log(1234);
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    console.log(req.body);
-    setTimeout(function () {
-        res.end();
-    },1000)
+    response.setHeader('Access-Control-Allow-Origin', 'hert');
+    //设置请求的返回头type,content的type类型列表见上面
+    response.setHeader("Content-Type", "image/png");
+//格式必须为 binary 否则会出错
+    var content = fs.readFileSync("./bg.png", "binary");
+    response.writeHead(200, "Ok");
+    response.write(content, "binary"); //格式必须为 binary，否则会出错
+    response.end();
+    response.end();
 });
 
-server.listen(8000);
+server.listen(63342);
