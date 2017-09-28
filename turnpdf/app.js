d7549cde2351a0299d8b5f0c3e0907ae86e91195
Robert <464554492@qq.com>
2017-09-28 14:13:42 +0800

    html->pdf

--- turnpdf/app.js
+++ turnpdf/app.js
@@ -4,42 +4,24 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var PDF = require('pdfkit');
+var Pdf = require('pdfkit');
 var fs = require('fs');
 var index = require('./routes/index');
 var users = require('./routes/users');
 var app = express();
-
+var im = require("imagemagick");
+var gm = require("gm");
+var imageMagick = gm.subClass({imageMagick: true});
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-// app.use(bodyParser.json({type:"parameters.too.many",  limit:"1000000009999mb"}));
-// app.use(bodyParser.urlencoded({type:"parameters.too.many", limit: "9999999kb", extended: true}));
-// app.use(bodyParser.text({type:"parameters.too.many", limit: "999999999kb", extended: true}));
-// app.use(bodyParser({limit: '50mb'}))
-// app.use(bodyParser.json({limit: '50mb'}));
-// app.use(bodyParser.urlencoded({limit: '50mb'}));
-var jsonParser = bodyParser.json({limit: 1024 * 1024 * 20, type: 'application/json'});
-var urlencodedParser = bodyParser.urlencoded({extended: true, limit: 1024 * 1024 * 20, type: 'application/x-www-form-urlencoding'})
-app.use(jsonParser);
-app.use(urlencodedParser);
-// app.use('/', index);
-// app.use('/users', users);
-
-// catch 404 and forward to error handler
-// app.use(function (req, res, next) {
-//     var err = new Error('Not Found');
-//     err.status = 404;
-//     next(err);
-// });
 
 
 app.all(function () {
@@ -50,19 +32,44 @@ app.all(function () {
 
 })
 app.post('/pdf', function (req, res) {
-    console.log(req.body.title);
-    var base64Data = req.body.imgData.replace(/^data:image\/png;base64,/, "");
-    base64Data = decodeURI(base64Data);
+    var base64Data = decodeURIComponent(req.body.img);
     var binaryData = new Buffer(base64Data, 'base64').toString('binary');
     fs.writeFile('./uploads/out.png', binaryData, 'binary', function (err) {
         if (err) {
             console.log(err);
         }
+
     });
     res.send('saved.');
 });
 
-// error handler
+var doc = new Pdf();
+doc.pipe(fs.createWriteStream("demo.pdf"));
+
+im.resize({
+    srcPath: 'out.png',
+    dstPath: 'out1.png',
+    width: 600
+}, function (err, stdout, stderr) {
+    if (err) throw err;
+    console.log('resized kittens.jpg to fit within 256x256px');
+    im.convert(['out1.png', '-crop', '655x780', './uploads/3.png'],
+        function (err, stdout) {
+            if (err) throw err;
+            console.log('stdout:', stdout);
+            fs.readdir("./uploads", function (err, files) {
+                files.shift()
+                console.log(files);
+                for (var i = 0; i < files.length; i++) {
+                    doc.image(`./uploads/${files[i]}`, 0, 0, {fit: [700, 780]})
+                        .addPage()
+                }
+                doc.end();
+            })
+        });
+});
+
+// error ha ndler
 // app.use(function (err, req, res, next) {
 //     // set locals, only providing error in development
 //     res.locals.message = err.message;
@@ -72,7 +79,6 @@ app.post('/pdf', function (req, res) {
 //     res.status(err.status || 500);
 //     res.render('error');
 // });
-
 
 app.listen(3000)
 
